09940b5682ea3d0fc95dcd7f608be313da68614f
bronxpostma13 <bronxpostma@gmail.com>
2025-11-02 14:45:11 +0100

    Oude stuff

---
+++ script.js
@@ -0,0 +1,25 @@
+console.log(1);
+//  console is een object, 
+// log is een function (method) met een argument
+// de dot notatie geeft aan dat de log function bij het console object hoort
+function clown(name){
+
+    // return [name, shoeSize]
+    return {
+        name, shoeSize
+    }
+}
+
+
+const car = function ( brand, color, topSpeed){
+
+return{
+    brand, color, topSpeed,
+}
+
+}
+const building = (floors, roofType, elevator) => {
+
+}
+
+console.log(clown('bomba', 80))
